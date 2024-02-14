8446f1f80c00d382be53f0a790f20b8ed4c4181a
Isabela Gomes Silva <isabela.gomes@programmers.com.br>
2024-02-14 09:28:27 -0300

    Inicio_v6

--- src/Backend/Tests/Controllers/UserControllerTests.cs
+++ src/Backend/Tests/Controllers/UserControllerTests.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Stories.API.Controllers;
+using Stories.Data.Context;
 using Stories.Data.Models;
 using Stories.Service.Services;
 
@@ -12,13 +14,9 @@ namespace Tests.Controllers
 {
     public class UserControllerTests
     {
-        private readonly Mock<UserService> _mock;
-        private readonly UsersController _controller;
-
+        
         public UserControllerTests()
         {
-            _mock = new Mock<UserService>();
-            _controller = new UsersController(_mock.Object);
         }
 
          List<UserDto> usersDto = new List<UserDto>
@@ -33,22 +31,14 @@ namespace Tests.Controllers
         public async void Get_ReturnnoContent_When_NocontainsElements()
         {
             // Arrange
-            _mock.Setup(c => c.Get()).ReturnsAsync(new List<UserDto>());
-            // Act
-            var result = await _controller.Get();
+            
             // Assert
-            Assert.IsType<NoContentResult>(result);
-
+            
         }
         [Fact]
         public async void Get_ReturnOK_When_ContainsElements()
         {
-            // Arrange
-            _mock.Setup(c => c.Get()).ReturnsAsync(usersDto) ;
-            // Act
-            var result = await _controller.Get();
-            // Assert
-            Assert.IsType<OkObjectResult>(result);
+            
         }
         
     }
