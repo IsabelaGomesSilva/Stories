373048ce04236dd3c0bffa654ab3796890208751
IsabelaGomesSilva <isabelagmssilva@gmail.com>
2024-02-14 10:14:45 -0300

    v6_UsersControllerTests

--- src/Backend/Tests/Controllers/UserControllerTests.cs
+++ src/Backend/Tests/Controllers/UserControllerTests.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Moq;
 using Stories.API.Controllers;
 using Stories.Data.Context;
 using Stories.Data.Models;
@@ -14,32 +9,37 @@ namespace Tests.Controllers
 {
     public class UserControllerTests
     {
+        private readonly DbContextOptions<DataContext> optionsBd;
+        public UserControllerTests() =>  optionsBd = new DbContextOptionsBuilder<DataContext>()
+                                                     .UseInMemoryDatabase(databaseName: "DBSTORIES").Options;
         
-        public UserControllerTests()
-        {
-        }
-
-         List<UserDto> usersDto = new List<UserDto>
-        {
-            new () { Id = 1 , Name = "Isabela" },
-            new () { Id = 2, Name = "Julia" },
-            new () { Id = 3, Name = "Carlos" }
-        };
-        
-
         [Fact]
         public async void Get_ReturnnoContent_When_NocontainsElements()
         {
-            // Arrange
-            
-            // Assert
-            
+            var context = new DataContext(optionsBd);
+            var controller = new UsersController(new UserService(context));
+            Assert.IsType<NoContentResult>( await controller.Get());
         }
+
         [Fact]
         public async void Get_ReturnOK_When_ContainsElements()
         {
-            
+            using(var context = new DataContext(optionsBd))
+            {
+                context.User.Add( new User{Name = "Isabela" });
+                context.User.Add( new User{Name = "Julia" });
+                context.User.Add( new User{Name = "Carol" });
+                context.User.Add( new User{Name = "João" });
+                await context.SaveChangesAsync();
+            }
+            using(var context = new DataContext(optionsBd))
+            {
+                var controller = new UsersController(new UserService(context));
+                var resultTest = Assert.IsType<OkObjectResult>( await controller.Get());
+                var users = resultTest.Value as IEnumerable<UserDto>;
+                Assert.NotNull(users);
+                Assert.Equal(4,users.Count());
+            }           
         }
-        
     }
 }
